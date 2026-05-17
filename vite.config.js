0d56f0bb9a711a56990afd39de3c2914df3c390f
gqrfield <oweng325@gmail.com>
2026-05-17 04:27:11 -0500

    fix css attempt 2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/social-connectedness/', // Add this line (include the trailing and leading slashes!)
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  base: '/social-connectedness/',
 })
